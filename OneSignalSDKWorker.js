b580e236cd6a5703c4b417fb44852984d94a6988
MayneILLIMINET <topalmaynetpl13@gmail.com>
2026-07-27 21:29:48 +0100

    Update OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,7 +1,1 @@
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
-
-self.addEventListener("install", (e) => self.skipWaiting());
-self.addEventListener("activate", (e) => self.clients.claim());
-self.addEventListener("fetch", (e) => {
-  e.respondWith(fetch(e.request).catch(()=> caches.match(e.request)));
-});
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js')
